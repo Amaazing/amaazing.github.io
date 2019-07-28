07fd5e3d2534141590ea12a61a04d54231308b53
Khan <Khan.Ahmed@city.ac.uk>
2019-07-28 01:48:23 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,424 @@
+var data_path = "./data.json";
+var data = {
+ "4" :
+	{
+		"rm": [
+			{"card": "Junior Detective", "primary": 4, "secondary":0},
+			{"card": "Decisive Clue", "primary": 2, "secondary":0},
+			{"card": "Sherlock Namjun", "primary": 4, "secondary":0},
+			{"card": "Collecting Evidence", "primary": 1, "secondary":0},
+			{"card": "Trench Coat", "primary": 1, "secondary":0},
+			{"card": "Handsome Dimple Guy", "primary": 2, "secondary":0},
+			{"card": "Kim Daily", "primary": 3, "secondary":0},
+			{"card": "Charisma", "primary": 3, "secondary":0},
+			{"card": "Found It", "primary": 4, "secondary":0},
+			{"card": "Red Thread", "primary": 4, "secondary":0},
+			{"card": "Clear Skies After Rain", "primary": 3, "secondary":0},
+			{"card": "Hideout", "primary": 4, "secondary":0},
+			{"card": "Birthday Party", "primary": 2, "secondary":0},
+			{"card": "Brimming with Confidence", "primary": 1, "secondary":0}
+		],
+		"jin": [
+			{"card": "Special Service", "primary": 1, "secondary":0},
+			{"card": "Looks Working Overtime ", "primary": 1, "secondary":0},
+			{"card": "Man with a Flower ", "primary": 2, "secondary":0},
+			{"card": "A Beaming Smile", "primary": 2, "secondary":0},
+			{"card": "Too Sweet", "primary": 3, "secondary":0},
+			{"card": "Broad-Shouldered", "primary": 3, "secondary":0},
+			{"card": "Checkmate ", "primary": 1, "secondary":0},
+			{"card": "Gotta Go", "primary": 2, "secondary":0},
+			{"card": "Deep in Thought ", "primary": 3, "secondary":0},
+			{"card": "Undercover ", "primary": 4, "secondary":0},
+			{"card": "Alone Time", "primary": 4, "secondary":0},
+			{"card": "A Sudden Jolt", "primary": 4, "secondary":0},
+			{"card": "Afternoon Break ", "primary": 1, "secondary":0},
+			{"card": "Man of the Year", "primary": 2, "secondary":0}
+		],
+		"suga": [
+			{"card": "Pure White Sugar", "primary": 2, "secondary":0},
+			{"card": "Many Worries", "primary": 4, "secondary":0},
+			{"card": "Metronome ", "primary": 1, "secondary":0},
+			{"card": "Bowtie", "primary": 3, "secondary":0},
+			{"card": "A Pianist ", "primary": 2, "secondary":0},
+			{"card": "An Autumn Letter", "primary": 3, "secondary":0},
+			{"card": "Young Master", "primary": 2, "secondary":0},
+			{"card": "Genius Composer ", "primary": 1, "secondary":0},
+			{"card": "Blonde with Beanie ", "primary": 2, "secondary":0},
+			{"card": "SUGA Rabbit", "primary": 4, "secondary":0},
+			{"card": "Sleepy Velvet ", "primary": 3, "secondary":0},
+			{"card": "Longing", "primary": 3, "secondary":0},
+			{"card": "Pearly Smile", "primary": 4, "secondary":0},
+			{"card": "Snowflake Boy", "primary": 1, "secondary":0}
+		],
+		"hobi": [
+			{"card": "Pretty Pretty", "primary": 1, "secondary":0},
+			{"card": "Sit, Wait", "primary": 2, "secondary":0},
+			{"card": "Great Work", "primary": 3, "secondary":0},
+			{"card": "Doctor Hope", "primary": 4, "secondary":0},
+			{"card": "Group Project", "primary": 1, "secondary":0},
+			{"card": "Eh, No Idea", "primary": 3, "secondary":0},
+			{"card": "Gentleman", "primary": 4, "secondary":0},
+			{"card": "Hobi's Great Adventure", "primary": 3, "secondary":0},
+			{"card": "Troublemaker", "primary": 1, "secondary":0},
+			{"card": "Choices and Concentration", "primary": 2, "secondary":0},
+			{"card": "Hip Hop Hope", "primary": 4, "secondary":0},
+			{"card": "First Greeting", "primary": 1, "secondary":0},
+			{"card": "Golden Afternoon", "primary": 3, "secondary":0},
+			{"card": "Dazzling Hope", "primary": 2, "secondary":0}
+		],
+		"jimin": [
+			{"card": "Playlist", "primary": 1, "secondary":0},
+			{"card": "Eye Contact", "primary": 2, "secondary":0},
+			{"card": "Sweet Pâtissier", "primary": 2, "secondary":0},
+			{"card": "Kendo Hopeful", "primary": 1, "secondary":0},
+			{"card": "Trivial Worries", "primary": 2, "secondary":0},
+			{"card": "So Sleepy", "primary": 3, "secondary":0},
+			{"card": "Bodyguard", "primary": 3, "secondary":0},
+			{"card": "Bright-Eyed", "primary": 4, "secondary":0},
+			{"card": "Cotton Fragance", "primary": 1, "secondary":0},
+			{"card": "Secret Recipe", "primary": 3, "secondary":0},
+			{"card": "Adorable Pâtissier", "primary": 4, "secondary":0},
+			{"card": "Shy Pâtissier", "primary": 4, "secondary":0},
+			{"card": "Lovely Pâtissier", "primary": 1, "secondary":0},
+			{"card": "Growing Pains", "primary": 4, "secondary":0}
+		],
+		"tae": [
+			{"card": "Shoking Visual", "primary": 1, "secondary":0},
+			{"card": "TaeTae's Laboratory", "primary": 2, "secondary":0},
+			{"card": "Brilliant Glasses", "primary": 3, "secondary":0},
+			{"card": "Cherry Blossom Opening", "primary": 2, "secondary":0},
+			{"card": "Time to Eat", "primary": 3, "secondary":0},
+			{"card": "It's Dangerous Out There", "primary": 4, "secondary":0},
+			{"card": "One Wish", "primary": 1, "secondary":0},
+			{"card": "Mr. Curiosity", "primary": 2, "secondary":0},
+			{"card": "Let's See", "primary": 3, "secondary":0},
+			{"card": "Bumper Tomato Crop", "primary": 4, "secondary":0},
+			{"card": "House Party", "primary": 2, "secondary":0},
+			{"card": "A Break", "primary": 1, "secondary":0},
+			{"card": "A Silent Night", "primary": 4, "secondary":0},
+			{"card": "Late-Night Feeling", "primary": 3, "secondary":0}
+		],
+		"jk": [
+			{"card": "Taekwon Boy", "primary": 1, "secondary":0},
+			{"card": "Black Belt", "primary": 3, "secondary":0},
+			{"card": "Ray of Sunshine", "primary": 2, "secondary":0},
+			{"card": "Cheerful Lad", "primary": 4, "secondary":0},
+			{"card": "Excited", "primary": 1, "secondary":0},
+			{"card": "Spaced Out", "primary": 3, "secondary":0},
+			{"card": "Red Sun", "primary": 2, "secondary":0},
+			{"card": "Naive", "primary": 1, "secondary":0},
+			{"card": "Board Game Time", "primary": 2, "secondary":0},
+			{"card": "Spring Breeze", "primary": 2, "secondary":0},
+			{"card": "5 Minutes 'Til the Match", "primary": 4, "secondary":0},
+			{"card": "Head-to-Head", "primary": 1, "secondary":0},
+			{"card": "Early Spring", "primary": 3, "secondary":0},
+			{"card": "A Novelist Reading", "primary": 2, "secondary":0}
+		]},
+	 "5" :
+	{
+		"rm" : [
+			{"card": "Private Eye",  "primary": 1, "secondary": 0},
+			{"card": "Smile", "primary": 2, "secondary": 0},
+			{"card": "Classic", "primary": 2, "secondary": 0},
+			{"card": "Professional", "primary": 1, "secondary": 0},
+			{"card": "Cherry Blossom RM", "primary": 3, "secondary": 0},
+			{"card": "Dreamland RM", "primary": 1, "secondary": 0},
+			{"card": "Romantic RM", "primary": 3, "secondary": 0},
+			{"card": "FAKE LOVE RM", "primary": 1, "secondary": 0},
+			{"card": "Red Carpet RM", "primary": 4, "secondary": 0}
+		],
+		"jin" : [
+			{"card": "You're Quite Handsome", "primary": 1, "secondary": 0},
+			{"card": "Worldwide Handsome", "primary": 3, "secondary": 0},
+			{"card": "Ready for Work", "primary": 2, "secondary": 0},
+			{"card": "Pleased to Meet You", "primary": 4, "secondary": 0},
+			{"card": "Cherry Blossom Jin", "primary": 4, "secondary": 0},
+			{"card": "Dreamland Jin", "primary": 2, "secondary": 0},
+			{"card": "Romantic Jin", "primary": 3, "secondary": 0},
+			{"card": "FAKE LOVE JIN", "primary": 4, "secondary": 0},
+			{"card": "Red Carpet Jin", "primary": 4, "secondary": 0}
+		],
+		"suga" : [
+			{"card": "Min MeowMeow on the Keyboard", "primary": 1, "secondary": 0},
+			{"card": "Twinkle Twinkl", "primary": 2, "secondary": 0},
+			{"card": "Adjacent Library Se", "primary": 4, "secondary": 0},
+			{"card": "The Decisive Momen", "primary": 3, "secondary": 0},
+			{"card": "Cherry Blossom Sug", "primary": 1, "secondary": 0},
+			{"card": "Dreamland Sug", "primary": 2, "secondary": 0},
+			{"card": "Romantic Sug", "primary": 3, "secondary": 0},
+			{"card": "FAKE LOVE SUG", "primary": 2, "secondary": 0},
+			{"card": "Red Carpet Sug", "primary": 4, "secondary": 0}
+		],
+		"hobi" : [
+			{"card": "See-Through Umbrella", "primary": 1, "secondary": 0},
+			{"card": "Smiling Angel", "primary": 1, "secondary": 0},
+			{"card": "Let's Play, Friend", "primary": 2, "secondary": 0},
+			{"card": "White-Coated Angel", "primary": 2, "secondary": 0},
+			{"card": "Cherry Blossom JHOPE", "primary": 3, "secondary": 0},
+			{"card": "Dreamland JHOPE", "primary": 4, "secondary": 0},
+			{"card": "Romantic JHOPE", "primary": 3, "secondary": 0},
+			{"card": "FAKE LOVE JHOPE", "primary": 3, "secondary": 0},
+			{"card": "Red Carpet JHOPE", "primary": 4, "secondary": 0}
+		],
+		"jimin" : [
+			{"card": "Alone in the Practice Room", "primary": 1, "secondary": 0},
+			{"card": "Dancing Mang-gae-ddeok", "primary": 2, "secondary": 0},
+			{"card": "World's Greatest Swordsman", "primary": 4, "secondary": 0},
+			{"card": "Nighttime Stroll", "primary": 3, "secondary": 0},
+			{"card": "Cherry Blossom Jimin", "primary": 2, "secondary": 0},
+			{"card": "Dreamland Jimin", "primary": 3, "secondary": 0},
+			{"card": "Romantic Jimin", "primary": 3, "secondary": 0},
+			{"card": "FAKE LOVE JIMIN", "primary": 4, "secondary": 0},
+			{"card": "Red Carpet Jimin", "primary": 4, "secondary": 0}
+		],
+		"tae" : [
+			{"card": "Wink", "primary": 1, "secondary": 0},
+			{"card": "Wise Guy", "primary": 2, "secondary": 0},
+			{"card": "I Purple You", "primary": 2, "secondary": 0},
+			{"card": "Orchard Boy", "primary": 3, "secondary": 0},
+			{"card": "Cherry Blossom V", "primary": 4, "secondary": 0},
+			{"card": "Dreamland V", "primary": 4, "secondary": 0},
+			{"card": "Romantic V", "primary": 3, "secondary": 0},
+			{"card": "FAKE LOVE V", "primary": 3, "secondary": 0},
+			{"card": "Red Carpet V", "primary": 4, "secondary": 0}
+		],
+		"jk" : [
+			{"card": "National Team Athlete", "primary": 1, "secondary": 0},
+			{"card": "The Name's JungKook", "primary": 1, "secondary": 0},
+			{"card": "To the Store", "primary": 2, "secondary": 0},
+			{"card": "Golden Maknae", "primary": 2, "secondary": 0},
+			{"card": "Cherry Blossom JungKook", "primary": 1, "secondary": 0},
+			{"card": "Dreamland JungKook", "primary": 2, "secondary": 0},
+			{"card": "Romantic JungKook", "primary": 3, "secondary": 0},
+			{"card": "FAKE LOVE JUNGKOOK", "primary": 2, "secondary": 0},
+			{"card": "Red Carpet JungKook", "primary": 4, "secondary": 0}
+		]
+	}
+};
+var user_cards = {}; // initialised in init()
+
+var [PRIMARY_5, SECONDARY_5] = [45, 30];
+var [PRIMARY_4, SECONDARY_4] = [39, 26];
+
+$( document ).ready(function() {
+	init();
+	load_user_cards(); // load any cards that are in storage
+	render_data_list(); // populate the datalist with names of all cards
+
+	events();
+});
+
+function init(){
+
+	// $.getJSON( "./data.json", function( _data ) {
+	//	 data = _data;
+	// });
+
+	user_cards.data = [];
+	user_cards.add = function(x){
+		this.data.push(x);
+	}
+	user_cards.last_card = function(){
+		let len = this.data.length;
+		if (len == 0){
+			return 0;
+		} else if (len == 1){
+			return this.data[0];
+		} else {
+			return this.data[this.data.length-1];
+		}
+	}
+	user_cards.last_index = function(){
+		let len = this.data.length;
+		if ( len == 0 ) return 0;
+		return len - 1;
+	}
+}
+
+
+function load_user_cards(){
+	function _populate_select_options(){
+		console.log("test")
+		$.each($(".card_slection_add"), function(i, selection){
+			user_cards.data.forEach( (card, i) => {
+				selection.append(new Option(card['card'], i));
+			});
+		});
+		
+	}
+
+	function _render_user_cards(){
+		user_cards.data.forEach((card) => _make_row(card));
+	}
+
+	let _user_cards = window.localStorage.getItem("cards");
+	if (_user_cards === null) return;
+
+	user_cards.data = JSON.parse(_user_cards); // add to global var
+
+	// add options to select tag to pick a card from storage
+	_render_user_cards();
+	_populate_select_options();
+}
+
+function render_data_list(){
+	let datalist = document.getElementById("cards_list_names");
+	let str = "";
+
+	Object.keys(data).forEach( (star) => {
+		Object.keys(data[`${star}`]).forEach( (character) => {
+			data[`${star}`][`${character}`].forEach( (card, i) => {
+				// console.log(card['card']);
+				let card_name = card['card'];
+				str += `<option 
+					value="[${star}][${character.toUpperCase()}] ${card_name}"
+					name="${character}"
+					character="${character}"
+					star="${star}"
+					i=${i}
+					>\n`;
+			});
+		});
+	});
+
+	datalist.innerHTML = str;	
+}
+
+function events(){
+	$( "#addCardSubmit" ).click(function(e) {
+		e.preventDefault();
+		console.log(`test`);
+		update_user_cards_view();
+		update_select_options();
+	});
+
+	$( "select.card_slection_add" ).change(function(e){
+		window.cat = e;
+		let card = $(e.currentTarget).children("option:selected")[0];
+		let card_id = $(e.currentTarget).parent().parent()[0].id
+		let empathy_label = $(`#${card_id} .empathy`);
+		let passion_label = $(`#${card_id} .passion`);
+		let stamina_label = $(`#${card_id} .stamina`);
+		let wisdom_label = $(`#${card_id} .wisdom`);
+
+		console.log(card.value);
+
+		let empathy_base_value = user_cards.data[card.value]["empathy"];
+		let passion_base_value = user_cards.data[card.value]["passion"];
+		let stamina_base_value = user_cards.data[card.value]["stamina"];
+		let wisdom_base_value = user_cards.data[card.value]["wisdom"];
+		let primary_stat = user_cards.data[card.value]["primary"];
+		let star = user_cards.data[card.value]["star"];
+
+		let input = $(`#${card_id} input`)[0];
+
+		empathy_label.text(empathy_base_value);
+		passion_label.text(passion_base_value);
+		stamina_label.text(stamina_base_value);
+		wisdom_label.text(wisdom_base_value);
+
+		input.disabled = false;
+		$(input).change(function (e){
+			let labels = [empathy_label, passion_label, stamina_label, wisdom_label];
+			let base_stats = [empathy_base_value, passion_base_value, stamina_base_value, wisdom_base_value];
+			let level = e.currentTarget.value;
+			let PRIMARY, SECONDARY;
+			if (star == 4) [PRIMARY, SECONDARY] = [PRIMARY_4, SECONDARY_4]
+			else [PRIMARY, SECONDARY] = [PRIMARY_5, SECONDARY_5]
+
+ 			for (var i = 0; i < labels.length; i++) {
+				let FACTOR = i === (primary_stat-1) ? PRIMARY : SECONDARY
+				labels[i].text(base_stats[i] + ((level-1) * FACTOR));
+			}
+		});
+
+	});
+
+	$("#clear").click(function(){
+		window.localStorage.removeItem("cards");
+		document.location.reload();
+	});
+}
+
+function _make_row(card){
+	let cell_count = 0;
+	let _table = document.querySelector("#userCards table");
+	let row = _table.insertRow(-1);
+
+	let star = row.insertCell(cell_count++);
+	let character = row.insertCell(cell_count++);
+	let primary = row.insertCell(cell_count++);
+	let name = row.insertCell(cell_count++);
+	let empathy = row.insertCell(cell_count++);
+	let passion = row.insertCell(cell_count++);
+	let stamina = row.insertCell(cell_count++);
+	let wisdom = row.insertCell(cell_count++);
+
+	star.innerHTML = card['star'];
+	character.innerHTML = card['character'];
+	primary.innerHTML = card['primary'];
+	name.innerHTML = card['card'];
+	empathy.innerHTML = card['empathy'];
+	passion.innerHTML = card['passion'];
+	stamina.innerHTML = card['stamina'];
+	wisdom.innerHTML = card['wisdom'];
+}
+
+function update_select_options(){
+	let last_card = user_cards.last_card();
+	let card_i = user_cards.last_index();
+	let name = `[${last_card['star']}][${last_card['character'].toUpperCase()}] ${last_card['card']}`;
+	$.each($(".card_slection_add"), function (i, select){
+		select.append(new Option(name, card_i));
+	});
+}
+
+function update_user_cards_view(){
+	function update_cards_view(){
+		_make_row(user_cards.last_card());
+	}
+
+	let empathy = parseInt($("input#empathy")[0].value);
+	let passion = parseInt($("input#passion")[0].value);
+	let stamina = parseInt($("input#stamina")[0].value);
+	let wisdom = parseInt($("input#wisdom")[0].value);
+	let level = parseInt($("input#level")[0].value);
+
+	let card_seletion = $("input#card_slection_names")[0].value;
+	let card_option = $('datalist#cards_list_names option').filter(function(){return this.value === card_seletion});
+	let star = $(card_option).attr("star");
+	let character = $(card_option).attr("character");
+	let card = data[`${star}`][`${character}`][`${card_option.attr("i")}`];
+	let primary = card['primary'];
+
+	if (level > 1){
+		let stats = [empathy, passion, stamina, wisdom];
+		let PRIMARY, SECONDARY;
+
+		if (star == 4) [PRIMARY, SECONDARY] = [PRIMARY_4, SECONDARY_4]
+		else [PRIMARY, SECONDARY] = [PRIMARY_5, SECONDARY_5]
+
+		for (var i = 0; i < stats.length; i++) {
+			let FACTOR = i === (primary-1) ? PRIMARY : SECONDARY
+			stats[i] -= ((level-1) * FACTOR);
+		}
+
+		empathy = stats[0];
+		passion = stats[1];
+		stamina = stats[2];
+		wisdom = stats[3];
+	}
+
+	card['empathy'] = empathy;
+	card['passion'] = passion;
+	card['stamina'] = stamina;
+	card['wisdom'] = wisdom;
+	card['star'] = star;
+	card['character'] = character;
+	card['primary'] = primary;
+
+
+	user_cards.add(card);
+	update_cards_view();
+	window.localStorage.setItem("cards", JSON.stringify(user_cards.data));
+}
