3d1f54315983f622106a98387e76165dd3d6b4cf
Khan <Khan.Ahmed@city.ac.uk>
2019-07-31 14:39:30 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -196,12 +196,14 @@ var data = {
 	}
 };
 var user_cards = {}; // initialised in init()
+var agency_scores = [];
 
 var [PRIMARY_5, SECONDARY_5] = [45, 30];
 var [PRIMARY_4, SECONDARY_4] = [39, 26];
 
 $( document ).ready(function() {
 	init();
+	load_agency_scores();
 	load_user_cards(); // load any cards that are in storage
 	render_data_list(); // populate the datalist with names of all cards
 
@@ -238,7 +240,6 @@ function init(){
 
 function load_user_cards(){
 	function _populate_select_options(){
-		console.log("test")
 		$.each($(".card_slection_add"), function(i, selection){
 			user_cards.data.forEach( (card, i) => {
 				selection.append(new Option(card['card'], i));
@@ -259,6 +260,16 @@ function load_user_cards(){
 	// add options to select tag to pick a card from storage
 	_render_user_cards();
 	_populate_select_options();
+}
+
+function load_agency_scores(){
+	let _agency_scores = window.localStorage.getItem("agency");
+	if (_agency_scores === null) return;
+	agency_scores = JSON.parse(_agency_scores);
+	$("#empathy0, #agencyLevels .empathy > label").text(agency_scores[0]);
+	$("#passion0, #agencyLevels .passion > label").text(agency_scores[1]);
+	$("#stamina0, #agencyLevels .stamina > label").text(agency_scores[2]);
+	$("#wisdom0, #agencyLevels .wisdom > label").text(agency_scores[3]);
 }
 
 function render_data_list(){
@@ -287,7 +298,6 @@ function render_data_list(){
 function events(){
 	$( "#addCardSubmit" ).click(function(e) {
 		e.preventDefault();
-		console.log(`test`);
 		update_user_cards_view();
 		update_select_options();
 	});
@@ -300,8 +310,6 @@ function events(){
 		let passion_label = $(`#${card_id} .passion`);
 		let stamina_label = $(`#${card_id} .stamina`);
 		let wisdom_label = $(`#${card_id} .wisdom`);
-
-		console.log(card.value);
 
 		let empathy_base_value = user_cards.data[card.value]["empathy"];
 		let passion_base_value = user_cards.data[card.value]["passion"];
@@ -334,9 +342,35 @@ function events(){
 
 	});
 
-	$("#clear").click(function(){
+	$("#clear").click(function(e){
+		e.preventDefault();
 		window.localStorage.removeItem("cards");
 		document.location.reload();
+	});
+
+	$("#agencyLevels input, #card0 input").change(function(e){
+		let level = e.currentTarget.value;
+		let label = $(e.currentTarget).parent().children("label")
+		let score = 87 + (level * 13);
+		label.text(score);
+	});
+
+	$("#agencyLevelsSubmit").click(function(e){
+		e.preventDefault();
+		let empathy = parseInt($("#agencyLevels .empathy > label").text());
+		let passion = parseInt($("#agencyLevels .passion > label").text());
+		let stamina = parseInt($("#agencyLevels .stamina > label").text());
+		let wisdom = parseInt($("#agencyLevels .wisdom > label").text());
+
+		let scores = [empathy, passion, stamina, wisdom];
+
+		window.localStorage.setItem("agency", JSON.stringify(scores));
+		agency_scores = scores;
+
+		$("#empathy0").text(empathy);
+		$("#passion0").text(passion);
+		$("#stamina0").text(stamina);
+		$("#wisdom0").text(wisdom);
 	});
 }
 
